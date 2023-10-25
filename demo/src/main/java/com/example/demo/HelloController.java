e0a9bbfb47caf942ab3b5bf2418fd7a7a501b796
Jeong TaeHee <th1563@naver.com>
2023-10-25 23:36:39 +0900

    콘솔 로그 추가

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
+		System.out.println("Welcome");
 		return "index";
 	}
 }

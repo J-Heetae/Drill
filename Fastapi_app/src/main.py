0d05c530c38334c01b81995dca728c42a62b5d3c
YuJinUk <wlsdnrzlzl@naver.com>
2023-10-30 16:09:12 +0900

    fix: port number

--- Fastapi_app/src/main.py
+++ Fastapi_app/src/main.py
@@ -106,4 +106,4 @@ async def imguploadtoaws(img_ids: str):
 
  
 if __name__ == "__main__":
-    uvicorn.run(app = "__main__:app", host="0.0.0.0", port = 8080, reload = True)
+    uvicorn.run(app = "__main__:app", host="0.0.0.0", port = 8000, reload = True)

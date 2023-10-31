1a467aa2e6ad34791ee5472b8463ab67dfb7300f
YuJinUk <wlsdnrzlzl@naver.com>
2023-10-31 14:56:32 +0900

    fix: file path

--- Fastapi_app/src/main.py
+++ Fastapi_app/src/main.py
@@ -54,11 +54,11 @@ def read_name(name: str, status: str):
 
 @app.get("/download/video/{objectname}/{filename}")
 def amazon_s3(objectname: str, filename: str):
-    client_s3.download_file(os.environ.get("S3_BUCKET"), f"Video/{objectname}.mp4", f"video/{filename}.mp4")
+    client_s3.download_file(str(os.environ.get("S3_BUCKET")), f"Video/{objectname}.mp4", f"video/{filename}.mp4")
     # current_directory = os.getcwd()  # 현재 작업 디렉토리 가져오기
     # file_path = os.path.join(current_directory, 'video', f'{filename}.mp4')
     with open(f'.video/{filename}.mp4', 'wb') as f:
-        client_s3.download_fileobj(os.environ.get("S3_BUCKET"), f"Video/{objectname}.mp4", f)
+        client_s3.download_fileobj(str(os.environ.get("S3_BUCKET")), f"Video/{objectname}.mp4", f)
     return {"status" : "success 200"}
 
 @app.get("api/videopath/download")

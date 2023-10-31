36c60497ee5acec55fe8a6f086e607727ea5399a
유진욱 <wlsdnrzlzl@naver.com>
2023-10-31 17:20:54 +0900

    Update Fastapi_app/src/main.py

--- Fastapi_app/src/main.py
+++ Fastapi_app/src/main.py
@@ -47,7 +47,7 @@ async def read_root():
     # check = check_model()
     # return {"Hello": "jenkinsWorld",
     #         "check": check}
-    return {"0" : os.environ.get("CREDENTIALS_ACCESS_KEY")}
+    return {"0" : "성공 기모링"}
 
 @app.get("/information", response_class=HTMLResponse)
 async def read_root():

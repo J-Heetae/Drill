0fbd29fcfb074cffa3186943bae872e02c8269f3
유진욱 <wlsdnrzlzl@naver.com>
2023-10-31 17:29:30 +0900

    Update Fastapi_app/src/main.py

--- Fastapi_app/src/main.py
+++ Fastapi_app/src/main.py
@@ -66,14 +66,15 @@ def read_name(name: str, status: str):
 @app.get("/download/video/{objectname}/{filename}")
 def amazon_s3(objectname: str, filename: str):
     file_path = f"video/{filename}.mp4"
-    file_path = file_path.split(".")
-    file_path = file_path[0] + file_path[1]
-    client_s3.download_file(os.environ.get("S3_BUCKET"), f"Video/{objectname}.mp4", file_path)
+    return {"bucket" : os.environ.get("S3_BUCKET"), 
+            "object" : f"Video/{objectname}.mp4",
+            "file" : file_path}
+    # client_s3.download_file(os.environ.get("S3_BUCKET"), f"Video/{objectname}.mp4", file_path)
     # current_directory = os.getcwd()  # 현재 작업 디렉토리 가져오기
     # file_path = os.path.join(current_directory, 'video', f'{filename}.mp4')
     # with open(f'.video/{filename}.mp4', 'wb') as f:
     #     client_s3.download_fileobj(str(os.environ.get("S3_BUCKET")), f"Video/{objectname}.mp4", f)
-    return {"status" : "file_path"}
+    # return {"status" : "file_path"}
 
 @app.get("api/videopath/download")
 async def videopath(video_ids : str):

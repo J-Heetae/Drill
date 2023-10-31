d04ce8da4b63e0e98dfc195e0345979662ac3b52
유진욱 <wlsdnrzlzl@naver.com>
2023-10-31 15:52:23 +0900

    Update Fastapi_app/src/main.py

--- Fastapi_app/src/main.py
+++ Fastapi_app/src/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
 )
 
 # 정적 파일을 서빙할 디렉토리를 설정
-app.mount("./static", StaticFiles(directory="./static"), name="static")
+app.mount("./static", StaticFiles(directory=".static"), name="static")
 load_dotenv() # .env 파일에 있는 키와 값을 환경변수로 등록해주는 library
 
 '''

302ac34ac534dcc45819d2b107b80613e0558035
minseok7746 <minseok77746@naver.com>
2023-11-07 16:05:38 +0900

    feat: add modal&mypage

--- drillapp/src/screens/Video.tsx
+++ drillapp/src/screens/Video.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
-import { FlatList, TextInput, StyleSheet, Image, Text, TouchableOpacity, Alert } from 'react-native';
+import { FlatList, TextInput, StyleSheet, Image, Text, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
 import { RootState } from "../modules/redux/RootReducer";
@@ -8,12 +8,18 @@ import { useSelector } from "react-redux";
 import AWS from 'aws-sdk';
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Dropdown } from 'react-native-element-dropdown';
 
 AWS.config.update({
   accessKeyId: 'AKIA32XVP6DS7XK33DGC',
   secretAccessKey: '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',
   region: 'ap-northeast-2',
 });
+type DataItem = {
+  key: string;
+  value: string;
+  disabled?: boolean;
+};
 interface Post {
   center: string;
   course: {
@@ -39,6 +45,15 @@ interface Post {
   postVideo: string;
   postWriteTime: string;
 }
+interface NextPageDto {
+  centerName: string;
+  courseName: string;
+  difficulty: string;
+  memberNickname: string;
+  order: string;
+  page: number;
+  size: number;
+}
 type RootStackParamList = {
   VideoDetail: {id: number};
 };
@@ -50,6 +65,37 @@ const Video = () => {
   const [text, setText] = useState('');
   const userInfo = useSelector((state: RootState) => state.templateUser);
   const navigation = useNavigation<StackNavigationProp<RootStackParamList, 'VideoDetail'>>();
+
+  const [selectedCenter, setSelectedCenter] = useState("center0");
+  const [selectedHolder, setSelectedHolder] = useState("difficulty0");
+  const [selectedCourse, setSelectedCourse] = useState("course0");
+  const [selectedCourseName, setSelectedCourseName] = useState([]);
+  const [transformedCourseName, setTransformedCourseName] = useState<{ label: string; value: string; }[]>([]);
+
+  const data: DataItem[] = [
+    {key:'center0',value:'전체'},
+    {key:'center1',value:'홍대'},
+    {key:'center2',value:'일산'},
+    {key:'center3',value:'양재'},
+    {key:'center4',value:'마곡'},
+    {key:'center5',value:'신림'},
+    {key:'center6',value:'연남'},
+    {key:'center7',value:'강남'},
+    {key:'center8',value:'사당'},
+    {key:'center9',value:'신사'},
+    {key:'center10',value:'서울대'},
+  ];
+  const holderColor: DataItem[] = [
+    {key:'difficulty0',value:'전체'},
+    {key:'difficulty1',value:'하양'},
+    {key:'difficulty2',value:'노랑'},
+    {key:'difficulty3',value:'주황'},
+    {key:'difficulty4',value:'초록'},
+    {key:'difficulty5',value:'하양'},
+    {key:'difficulty6',value:'노랑'},
+    {key:'difficulty7',value:'주황'},
+    {key:'difficulty8',value:'초록'},
+  ];
 
   const onChangeText = (inputText: string) => {
     setText(inputText);
@@ -87,16 +133,16 @@ const Video = () => {
   // --------------------------------------------
 
   const API_URL = 'http://10.0.2.2:8060/api/post/list';
-
+  const [currentPage, setCurrentPage] = useState(0);
   // EntirePostPageDto 객체 생성
   const entirePostPageDto = {
-    centerName: 'center0',
-    courseName: 'course0',
-    difficulty: 'difficulty0',
-    memberNickname: '',
+    centerName: selectedCenter,
+    courseName: selectedCourse,
+    difficulty: selectedHolder,
+    memberNickname: text,
     order: 'sss',
     page: 0,
-    size: 10,
+    size: 12,
   };
 
   // POST 요청 보내기
@@ -109,7 +155,10 @@ const Video = () => {
       });
       // 성공
       setPosts(response.data.postPage.content);
-      console.log('게시글 가져오는데 성공',response.data)
+      console.log('게시글 가져오는데 성공',response);
+      console.log('지점 + 홀드에 대한 코스정보 리스트-----', response.data.courseNameList);
+      setSelectedCourseName(response.data.courseNameList);
+      console.log('POSTDTO---------', entirePostPageDto);
     } catch (error) { 
       // 요청
       console.error('게시글 목록을 불러오는 데 실패:', error);
@@ -141,11 +190,56 @@ const Video = () => {
     searchPosts();
   };
 
+  // 무한스크롤
+  const endPoint = () => {
+    // 현재 페이지 번호를 증가시킵니다.
+    setCurrentPage(currentPage + 1);
+
+    // 다음 페이지의 데이터를 불러오기 위한 요청을 보냅니다.
+    const nextPageDto = {
+      ...entirePostPageDto,
+      page: currentPage + 1,
+    };
+    // 다음 페이지의 데이터를 불러오는 요청 보내기
+    fetchNextPageData(nextPageDto);
+  };
+  const fetchNextPageData = async (nextPageDto: NextPageDto) => {
+    try {
+      const response = await axios.post(API_URL, nextPageDto, {
+        headers: {
+          Authorization: userInfo.accessToken,
+        },
+      });
+  
+      // 응답 데이터에 postPage가 존재하고 content 프로퍼티도 존재하는지 확인
+      if (response.data.postPage && response.data.postPage.content) {
+        // 이전 페이지의 데이터와 새로 불러온 데이터를 합칩니다.
+        setPosts([...posts, ...response.data.postPage.content]);
+        console.log('다음 페이지 데이터 가져오기 성공', response.data);
+      } else {
+        // 만약 postPage가 없거나 content가 없으면 더 이상 불러올 데이터가 없다고 판단할 수 있습니다.
+        console.log('더 이상 불러올 데이터가 없습니다.');
+      }
+    } catch (error) {
+      console.error('다음 페이지 데이터를 불러오는 데 실패:', error);
+    }
+  };
+  
 
   useEffect(() => {
     // 컴포넌트가 마운트될 때 데이터를 불러오기 위해 useEffect를 사용합니다.
     fetchPostList();
   }, []);
+  useEffect(() => {
+    fetchPostList();
+  }, [selectedCenter, selectedCourse, selectedHolder]);
+  useEffect(() => {
+    const transformed = selectedCourseName.map((course) => ({
+      label: course,
+      value: course, // or provide unique identifier for value
+    }));
+    setTransformedCourseName(transformed);
+  }, [selectedCourseName]);
 
   console.log('저장된값 출력하기-----------------------',posts)
   return(
@@ -163,30 +257,85 @@ const Video = () => {
           </TouchableOpacity>
         </SearchView>
         <SortMenuView>
-          <SortMenu><Text>지점</Text></SortMenu>
-          <SortMenu><Text>색</Text></SortMenu>
-          <SortMenu><Text>최신순</Text></SortMenu>
+          <SortMenu>
+            <Dropdown 
+              style={styles.dropdown1}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              mode='default'
+              data={data}
+              maxHeight={200}
+              placeholder="지점"
+              labelField="value"
+              valueField="key"
+              value={selectedCenter}
+              onChange={(item) => {
+                const selectedOption = data.find(option => option.value === item.value);
+                setSelectedCenter(selectedOption?.key || ''); // 선택된 항목을 찾아 상태 업데이트
+              }}
+            />
+          </SortMenu>
+          <SortMenu>
+            <Dropdown 
+              style={styles.dropdown2}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              mode='default'
+              data={holderColor}
+              maxHeight={200}
+              placeholder="홀드"
+              labelField="value"
+              valueField="key"
+              value={selectedHolder}
+              onChange={(item) => {
+                const selectedOption = holderColor.find(option => option.value === item.value);
+                setSelectedHolder(selectedOption?.key || ''); // 선택된 항목을 찾아 상태 업데이트
+              }}
+            />
+          </SortMenu>
+          <SortMenu>
+            <Dropdown 
+              style={styles.dropdown2}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              mode='default'
+              data={transformedCourseName}
+              maxHeight={200}
+              placeholder="코스"
+              labelField="label" // labelField 설정
+              valueField="value" // valueField 설정
+              value={selectedCourse}
+              onChange={ (item) => {
+                const selectedOption = transformedCourseName.find(option => option.value === item.value);
+                setSelectedCourse(selectedOption?.label || ''); // 선택된 항목을 찾아 상태 업데이트
+              }}
+            />
+          </SortMenu>
         </SortMenuView>
       </TopView>
 
       <BottomView>
         <SafeAreaView>
-        <FlatList
-          data={posts} // posts 배열을 데이터로 설정
-          renderItem={({ item }) => {
-            // 각 항목의 postThumbnail 값을 S3 URL로 변환
-            const imageUrl = `https://drill-video-bucket.s3.ap-northeast-2.amazonaws.com/Thumbnail/${item.postThumbnail}`;
-            
-            // 이미지를 화면에 표시하는 TouchableOpacity 컴포넌트 반환
-            return (
-              <TouchableOpacity onPress={() => navigation.navigate("VideoDetail", {id: item.postId})}>
-                <Image source={{ uri: imageUrl }} style={{ width: 100, height: 100, margin: 1 }} />
-              </TouchableOpacity>
-            );
-          }}
-          keyExtractor={(item) => item.postId.toString()} // 각 항목의 postId를 key로 사용
-          numColumns={3}
-        />
+          <FlatList
+            data={posts} // posts 배열을 데이터로 설정
+            renderItem={({ item }) => {
+              // 각 항목의 postThumbnail 값을 S3 URL로 변환
+              const imageUrl = `https://drill-video-bucket.s3.ap-northeast-2.amazonaws.com/Thumbnail/${item.postThumbnail}`;
+              
+              // 이미지를 화면에 표시하는 TouchableOpacity 컴포넌트 반환
+              return (
+                <TouchableOpacity onPress={() => navigation.navigate("VideoDetail", {id: item.postId})}>
+                  <Image source={{ uri: imageUrl }} style={{ width: 100, height: 100, margin: 1 }} />
+                </TouchableOpacity>
+              );
+            }}
+            keyExtractor={(item) => item.postId.toString()} // 각 항목의 postId를 key로 사용
+            numColumns={3}
+            onEndReached={endPoint}
+          />
         </SafeAreaView>
         
       </BottomView>
@@ -212,7 +361,33 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
     width: '100%',
     flex: 1,
-  }
+  },
+  dropdown1: {
+    width: 90,
+    height: 30,
+    backgroundColor: '#5AC77C',
+    borderRadius: 50,
+  },
+  dropdown2: {
+    width: 65,
+    height: 30,
+    backgroundColor: '#5AC77C',
+    borderRadius: 50,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
 });
 
 const ContainerView = styled.View`

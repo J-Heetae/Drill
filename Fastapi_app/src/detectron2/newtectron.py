deaadec0f52dd7a1659a8e9793806f93521194e9
lsw98 <sangwoo9808@gmail.com>
2023-11-13 02:38:44 +0900

    feat : hsv 변환
    Former-commit-id: d3fa9211866d1095d5d79f0a96857716300a0e2a

--- Fastapi_app/src/detectron2/newtectron.py
+++ Fastapi_app/src/detectron2/newtectron.py
@@ -7,6 +7,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
+import colorsys
 
 MODEL_DIRECTORY = os.getcwd() + "\\" + "model\\"
 # MODEL_DIRECTORY = os.getcwd() + "\\" + "detectron2\\" + "model\\"
@@ -23,19 +24,27 @@ metadata = MetadataCatalog.get("meta")
 
 predictor = DefaultPredictor(cfg)
 
-bgr_palette = {
-  "빨강" : (83, 75, 249),
-  "검정" : (21, 22, 20),
-  "노랑" : (6, 208, 227),
-  "보라" : (139, 68, 88),
-  "하양" : (224, 239, 237),
-  "초록" : (139, 170, 57),
-  "핑크" : (216, 81, 158),
-  "파랑" : (167, 64, 1),
-  "주황" : (1, 124, 231),
-  "민트" : (200, 181, 93),
-  "연두" : (25, 187, 142)
+
+def bgr_to_hsv(bgr):
+  bgr_np = np.array([[[bgr[0], bgr[1], bgr[2]]]], dtype= np.uint8)
+  bgr_hsv = cv2.cvtColor(bgr_np, cv2.COLOR_BGR2HSV).tolist()
+  return bgr_hsv[0][0]
+
+hsv_palette = {
+  "빨강" : bgr_to_hsv([83, 75, 249]),
+  "검정" : bgr_to_hsv([21, 22, 20]),
+  "노랑" : bgr_to_hsv([6, 208, 227]),
+  "보라" : bgr_to_hsv([139, 68, 88]),
+  "하양" : bgr_to_hsv([224, 239, 237]),
+  "초록" : bgr_to_hsv([139, 170, 57]),
+  "핑크" : bgr_to_hsv([11, 52, 227]),
+  "파랑" : bgr_to_hsv([167, 64, 1]),
+  "주황" : bgr_to_hsv([1, 124, 231]),
+  "민트" : bgr_to_hsv([200, 181, 93]),
+  "연두" : bgr_to_hsv([25, 187, 142]),
 }
+
+  
 
 def get_hold_info(img):
   outputs = predictor(img)
@@ -61,26 +70,32 @@ def get_hold_info(img):
   # tensor(좌측 상단 x 좌표, 좌측 상단 y 좌표, 우측 하단 x좌표, 우측 하단 y 좌표)
   # [hold/volume, 좌측상단x, 좌측상단y, 우측하단x, 우측하단y, (r, g, b)]] 형태로 저장
   
-
+  
+  
+  
+  # 이미지 HSV로 변환하여 평균 값 출력
+  hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
   
   box_info = []
   masks = outputs["instances"].pred_masks.tolist()
   for i in range(len(outputs["instances"].pred_boxes)):
-    red = 0
-    green = 0
-    blue = 0
-    rgb_cnt = 0
+    h = 0
+    s = 0
+    v = 0
+    hsv_cnt = 0
     mask = masks[i]
     x_length = len(mask)
     y_length = len(mask[0])
+    
+    # 마스크에서 HSV 값 추출
     for x in range(x_length):
       for y in range(y_length):
         if mask[x][y]:
-          bgr_color = img[x, y]
-          red += bgr_color[2]
-          green += bgr_color[1]
-          blue += bgr_color[0]
-          rgb_cnt += 1
+          hsv_color = hsv_img[x, y]
+          h += hsv_color[0]
+          s += hsv_color[1]
+          v += hsv_color[2]
+          hsv_cnt += 1
     coordinates = outputs["instances"].pred_boxes[i].tensor.tolist()[0]
     # 좌표 추출
     top_left_x = coordinates[0]
@@ -95,29 +110,39 @@ def get_hold_info(img):
     # 종류 추출
     category = "volume" if outputs["instances"].pred_classes[i] else "hold"
     
-    # RGB 추출
-    bgr_color = (int(blue/rgb_cnt), int(green/rgb_cnt), int(red/rgb_cnt))
+    # 평균 HSV값 추출
+    hsv_color = (int(h/hsv_cnt), int(s/hsv_cnt), int(v/hsv_cnt))
     
     predicted_color = ""
     dist = 9999999
     
-    # 유클리드 거리로 가장 가까운 색 찾기
-    for key, value in bgr_palette.items():
-      euc_dist = 0
-      for i in range(3):
-        euc_dist += (bgr_color[i] - value[i]) ** 2
-      if euc_dist < dist:
+    # H 0.5, S 0.25, V 0.25 가중치로 유사도 계산
+    for key, value in hsv_palette.items():
+      hsv_dist = 0
+      hsv_dist += abs(hsv_color[0] - value[0]) * 0.5 + abs(hsv_color[1] - value[1]) * 0.25 + abs(hsv_color[2] - value[2]) * 0.25
+      if hsv_dist < dist:
         predicted_color = key
-        dist = euc_dist
+        dist = hsv_dist
+    
+    
+    # # 유클리드 거리로 가장 가까운 색 찾기
+    # for key, value in bgr_palette.items():
+    #   euc_dist = 0
+    #   for i in range(3):
+    #     euc_dist += (hsv_color[i] - value[i]) ** 2
+    #   if euc_dist < dist:
+    #     predicted_color = key
+    #     dist = euc_dist
 
-    box_info.append([category, top_left_x, top_left_y, bottom_right_x, bottom_right_y, bgr_color, predicted_color])
+    box_info.append([category, top_left_x, top_left_y, bottom_right_x, bottom_right_y, hsv_color, predicted_color])
 
   # 이미지에 추출한 rgb 그려서 출력
-  img2 = copy.deepcopy(img)
-  img3 = copy.deepcopy(img)
+  img2 = copy.deepcopy(hsv_img)
+  img3 = copy.deepcopy(hsv_img)
   for box in box_info:
     cv2.rectangle(img=img2, pt1=(int(box[1]), int(box[2])), pt2=(int(box[3]), int(box[4])), color=box[5], thickness=-1)
-    cv2.rectangle(img=img3, pt1=(int(box[1]), int(box[2])), pt2=(int(box[3]), int(box[4])), color=bgr_palette[box[6]], thickness=-1)
+    cv2.rectangle(img=img3, pt1=(int(box[1]), int(box[2])), pt2=(int(box[3]), int(box[4])), color=hsv_palette[box[6]], thickness=-1)
+    print(box[6])
   
   fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
   ax1.imshow(img[:, :, ::-1])

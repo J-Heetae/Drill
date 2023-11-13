a2bd5ca968acf3cf14747327901f2e2849660833
YuJinUk <wlsdnrzlzl@naver.com>
2023-11-13 16:12:56 +0900

    check: hold color
    Former-commit-id: a82324a7365522abe35705ee54061a6d35246275

--- Fastapi_app/src/main.py
+++ Fastapi_app/src/main.py
@@ -97,7 +97,7 @@ async def remove_video(filename: str):
     return JSONResponse(content = content)
 
 @app.get("/video/process/{filename}")
-async def process_video(filename: str, request): # docker container에 저장된 동영상 파일 cv2로 실행되는 지 확인
+async def process_video(filename: str): # docker container에 저장된 동영상 파일 cv2로 실행되는 지 확인
     # hold_color = ut.get_params(request) # get filename in request
     # now_path = docker_container_path_check() # get current path
     # file_path = os.path.join(now_path, f"{filename}.mp4")

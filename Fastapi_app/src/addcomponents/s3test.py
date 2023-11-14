947290bc9bf54bd49da16c7974489b697d0f12e5
YuJinUk <wlsdnrzlzl@naver.com>
2023-11-14 16:06:51 +0900

    fix: video/remove api
    Former-commit-id: 0dd3e156c8128bfd7b47315f4fab1782aadfa0f2

--- Fastapi_app/src/addcomponents/s3test.py
+++ Fastapi_app/src/addcomponents/s3test.py
@@ -1,43 +1,53 @@
-# import boto3
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv() # use environment variables
-
-# client_s3 = boto3.client(
-#     's3',
-#     aws_access_key_id = os.environ.get("CREDENTIALS_ACCESS_KEY"),
-#     aws_secret_access_key = os.environ.get("CREDENTIALS_SECRET_KEY")
-# )
-
-# res = client_s3.delete_objects(Bucket = os.environ.get("S3_BUCKET"),
-#                               Delete = {
-#                                   'Objects': [
-#                                       {
-#                                           'Key': 'Thumbnail/test1.jpg'
-#                                       }
-#                                   ],
-#                                   'Quiet': False,
-#                               })
-
-# print(res)
-
 import cv2
-img = cv2.imread("974.jpg")
-img = cv2.resize(img, (450, 450))
-x, y = 200, 400
-print(img[y, x])
-# 이미지를 그레이스케일로 변환
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+import mediapipe as mp
 
-# 이미지의 밝기 값을 조정
-# adjusted = cv2.equalizeHist(img)
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+video_path = "../video/test13.mp4"
 
-# 이미지의 잡음 제거
-denoised = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+cap = cv2.VideoCapture(video_path)
+fps = cap.get(cv2.CAP_PROP_FPS) // 3
+frame_num = 0
 
-print(denoised[y, x])
+with mp_pose.Pose(
+    static_image_mode=True,
+    min_detection_confidence=0.3,
+    min_tracking_confidence=0.4,
+    model_complexity=2) as pose:
 
-cv2.imshow('img', img)
-cv2.imshow('denoised', denoised)
-cv2.waitKey(0)
+    while True:
+        success, image = cap.read()
+
+        if not success:
+            print("동영상을 찾을 수 없습니다.")
+            break
+
+        frame_num += 1
+        if frame_num % fps:  # 프레임 처리할 개수 설정
+            continue
+
+        image = cv2.resize(image, (450, 450))
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.pose_landmarks is not None:
+            landmarks = results.pose_landmarks.landmark
+            # 랜드마크 15번과 16번 좌표를 얻어옴
+            landmark_15 = (int(landmarks[mp_pose.PoseLandmark.LEFT_WRIST].x * image.shape[1]),
+                           int(landmarks[mp_pose.PoseLandmark.LEFT_WRIST].y * image.shape[0]))
+            landmark_16 = (int(landmarks[mp_pose.PoseLandmark.RIGHT_WRIST].x * image.shape[1]),
+                           int(landmarks[mp_pose.PoseLandmark.RIGHT_WRIST].y * image.shape[0]))
+
+            # 15번과 16번 랜드마크만 점으로 그리기
+            cv2.circle(image, landmark_15, 5, (0, 255, 0), -1)
+            cv2.circle(image, landmark_16, 5, (0, 255, 0), -1)
+
+        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()

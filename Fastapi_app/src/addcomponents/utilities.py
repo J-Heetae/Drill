a906b0038510a19382840a26a984fb9758a3d06a
유진욱 <wlsdnrzlzl@naver.com>
2023-11-20 12:37:10 +0900

    Update Fastapi_app/src/addcomponents/utilities.py
    Former-commit-id: 620ef3176b8af9b456c0b1cfea443685800c200d

--- Fastapi_app/src/addcomponents/utilities.py
+++ Fastapi_app/src/addcomponents/utilities.py
@@ -105,7 +105,7 @@ def video_process(video_name, hold_color): # Function to extract the location of
     print(video_path)
 
     cap = cv2.VideoCapture(video_path)
-    fps = cap.get(cv2.CAP_PROP_FPS) // 3
+    fps = cap.get(cv2.CAP_PROP_FPS) // 2
     cnt, frame_num = 0, 0
     wrist_all = []
     
